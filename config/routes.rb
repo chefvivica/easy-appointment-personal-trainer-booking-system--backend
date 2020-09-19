48473081001ffbe1c83b16c109971ed966dce674
Vivica <Ma>
2020-09-19 01:56:53 -0400

    trainer login

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :users
   post "/login", to: "auth#login"
   get "/auto_login", to: "auth#auto_login"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
