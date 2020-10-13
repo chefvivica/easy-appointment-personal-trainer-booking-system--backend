79477eab74ed2674907a574c07da49eab8cd72cd
Vivica <Ma>
2020-10-12 21:38:41 -0400

    added trainer passowrd to trainer serializer

--- app/serializers/trainer_serializer.rb
+++ app/serializers/trainer_serializer.rb
@@ -1,4 +1,4 @@
 class TrainerSerializer < ActiveModel::Serializer
-  attributes :id, :image, :name, :bio, :sports
+  attributes :id, :image, :name, :bio, :sports,:password_digest
   has_many :events
 end
